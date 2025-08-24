229facee1f20c8cd73cbbb168da319976fe95d55
Junichi Ito <jit@sonicgarden.jp>
2025-08-24 09:29:21 +0900

    日報を管理できるように / viewの整理

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,70 +1,53 @@
+# frozen_string_literal: true
+
 class ReportsController < ApplicationController
-  before_action :set_report, only: %i[ show edit update destroy ]
+  before_action :set_report, only: %i[edit update destroy]
 
-  # GET /reports or /reports.json
   def index
-    @reports = Report.all
+    @reports = Report.includes(:user).order(id: :desc).page(params[:page])
   end
 
-  # GET /reports/1 or /reports/1.json
   def show
+    @report = Report.find(params[:id])
   end
 
-  # GET /reports/new
   def new
     @report = Report.new
   end
 
-  # GET /reports/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /reports or /reports.json
   def create
-    @report = Report.new(report_params)
+    @report = current_user.reports.new(report_params)
 
-    respond_to do |format|
-      if @report.save
-        format.html { redirect_to @report, notice: "Report was successfully created." }
-        format.json { render :show, status: :created, location: @report }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
-      end
+    if @report.save
+      redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /reports/1 or /reports/1.json
   def update
-    respond_to do |format|
-      if @report.update(report_params)
-        format.html { redirect_to @report, notice: "Report was successfully updated." }
-        format.json { render :show, status: :ok, location: @report }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
-      end
+    if @report.update(report_params)
+      redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /reports/1 or /reports/1.json
   def destroy
     @report.destroy!
 
-    respond_to do |format|
-      format.html { redirect_to reports_path, status: :see_other, notice: "Report was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to reports_path, status: :see_other, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_report
-      @report = Report.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def report_params
-      params.expect(report: [ :user_id, :title, :content ])
-    end
+  def set_report
+    @report = current_user.reports.find(params[:id])
+  end
+
+  def report_params
+    params.expect(report: %i[user_id title content])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :reports
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
   resources :books
+  resources :reports
   resources :users, only: %i(index show)
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
